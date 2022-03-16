50320538047e8cc8126f60fd502dba16d2d87611
Yaroslav Tsepkov <yaroslav.cepkov@zimad.com>
2022-03-16 22:22:25 +0400

    add depends basic repos msapps scripts

--- run.sh
+++ run.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 chmod -Rv 777 basic
+echo 'tuning dependencies'
+./basic/dependencies.sh
+echo 'tuning repos'
+./basic/repos.sh
+echo 'tuning basic'
+./basic/basic.sh
+echo 'tuning microsoft apps'
+./basic/msapps.sh
+echo 'cleaning'
+apt autoremove
+echo 'finish'
